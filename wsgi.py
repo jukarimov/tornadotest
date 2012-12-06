6c82f9ce70cbf8b6400abd595b4683cc6eecfaa9
Jalil Karimov <jukarimov@gmail.com>
2012-12-06 22:31:42 +0500

    url added

--- wsgi.py
+++ wsgi.py
@@ -92,5 +92,6 @@ if __name__=='__main__':
   app = tornado.web.Application(routes, **settings)
   http_server = tornado.httpserver.HTTPServer(app)
   http_server.listen(8000)
+  print 'http://localhost:8000'
   tornado.ioloop.IOLoop.instance().start()
 
