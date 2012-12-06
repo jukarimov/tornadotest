772a2a02cf7c0ff387999bbe439243a7f9f5acbf
Jalil Karimov <jukarimov@gmail.com>
2012-12-06 22:28:49 +0500

    Editing fix

--- addbook.sql
+++ addbook.sql
@@ -3,16 +3,20 @@ create or replace function addbook(text,text,text) returns void as
 $BODY$
 DECLARE
 idcount int;
+idcount2 int;
 myid alias for $1;
 mynm alias for $2;
 myau alias for $3;
 BEGIN
-	idcount = (SELECT COUNT(*) FROM books WHERE books.id=myid);
-	IF (idcount < 1)
+	IF (myid <> '' AND mynm <> '')
 	THEN
-		IF (myid <> '' AND mynm <> '')
+		idcount = (SELECT COUNT(*) FROM books WHERE books.id=myid);
+
+		IF (idcount < 1)
 		THEN
 			INSERT INTO books VALUES(myid, mynm, myau);
+		ELSE
+			UPDATE books SET name=mynm, author=myau WHERE id=myid;
 		END IF;
 	END IF;
 END;
